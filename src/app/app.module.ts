0cd0901c33d2a1f3a3bb0c8d7fbe6f445100689f
Georgi Sotirov <sotirovgym@abv.bg>
2023-07-27 10:20:47 +0300

    I changed the default nav-bar buttons with ones related to our app. I added rounting to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import {Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from "./property/property-card/property-card.component";
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './add-property/add-property.component';
+
+const appRoutes: Routes = [
+  {path: '', component: PropertyListComponent},
+  {path: 'add-property', component: AddPropertyComponent}
+]
 
 @NgModule({
     declarations: [
         AppComponent,
         PropertyCardComponent,
         PropertyListComponent,
-        NavBarComponent
+        NavBarComponent,
+        AddPropertyComponent
    ],
     providers: [
       HousingService
@@ -21,7 +29,8 @@ import { HousingService } from './services/housing.service';
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes)
     ]
 })
 export class AppModule { }
